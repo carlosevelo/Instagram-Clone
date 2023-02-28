f8435170cd330f6adf2fefb04bdfb8d37f4bcb62
Carlos Evelo <carlosevelo96@gmail.com>
2023-02-27 19:30:33 -0700

    Initial Commit

---
+++ Instagram Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram Clone
+//
+//  Created by Carlos Evelo on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
